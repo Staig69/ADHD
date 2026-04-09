0f538e6a6fb22926476bfedbd5310e843697f47f
bty0tm <sibir22222@gmail.com>
2026-04-09 16:42:00 +0700

    Testing upload

---
+++ process_block.py
@@ -0,0 +1,10 @@
+import psutil
+from config import BLOCK_APPS
+
+def kill_apps():
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] in BLOCK_APPS:
+            try:
+                proc.kill()
+            except:
+                pass
---
+++ sites_block.py
@@ -0,0 +1,29 @@
+import pygetwindow as gw
+import pyautogui
+import pyperclip
+import time
+from config import BLOCK_SITES, REDIRECT_URL
+
+
+def redirect_browser():
+    for window in gw.getAllWindows():
+        title = window.title.lower()
+
+        if "block_page" in title:
+            continue
+
+        if any(s.lower() in title for s in BLOCK_SITES):
+            if any(b in window.title for b in ["Chrome", "Firefox", "Edge", "Opera"]):
+                try:
+                    window.activate()
+                    time.sleep(0.5)
+
+                    pyperclip.copy(REDIRECT_URL)
+
+                    pyautogui.press('f6')
+                    pyautogui.hotkey('shift', 'insert')
+                    pyautogui.press('enter')
+
+                    print(f"Пользователь отправлен учиться: {window.title}")
+                except Exception as e:
+                    print(f"Ошибка: {e}")
